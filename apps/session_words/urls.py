b6686db2fa67e0dbbc483f3691f88aaabbb8de97
Blaise James <blaise@blaisejames.com>
2018-06-15 14:59:17 -0400

    second commit of session_words

--- apps/session_words/urls.py
+++ apps/session_words/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'session_words/$', views.index),
+    url(r'session_words/$', views.index, name='index'),
     url(r'session_words/add_word$', views.add_word),
     url(r'session_words/clear$', views.clear),
 ]
--- apps/session_words/views.py
+++ apps/session_words/views.py
@@ -2,31 +2,29 @@ from django.shortcuts import render, HttpResponse, redirect
 from time import gmtime, strftime
 
 def index(request):
-    if 'sessionid' not in request.session:
-        request.session['sessionid'] = []
-    print request.session['sessionid']
-    return render(request, "session_words/index.html")
+    if 'words' not in request.session:
+        request.session['words'] = []
+    words = request.session.get('words')
+    context = {"words": words}
+    return render(request, "session_words/index.html", context)
 
 def add_word(request):
     if request.method == "GET":
         return render(request, "session_words/add.html")
     elif request.method == "POST":
         print request.POST
-        # if request.POST['fontsize'] == 'big':
-        #     size = "1.5em"
-        # else:
-        #     size = "1em"
         context = {
         "date": strftime("%b %d, %Y"),
 	    "time": strftime("%I:%M %p", gmtime()),
         "word": request.POST['word'],
         "color": request.POST['color'],
-        # "size": request.POST['fontsize'],
-        "user": request.session['sessionid']
+        "fontsize": request.POST['fontsize'],
         }
-        print context
-        return render(request, "session_words/index.html", context)
+        words = request.session.get('words', [])
+        words.append(context)
+        request.session['words'] = words
+        return redirect("index")
     
 def clear(request):
-    del request.session['sessionid']
-    return redirect("/")
+    del request.session['words']
+    return redirect("index")
